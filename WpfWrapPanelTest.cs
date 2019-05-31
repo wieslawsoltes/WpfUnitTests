55bc473f725937aea42a5000d1ddfe377151f0cf
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-31 18:04:40 +0200

    Update WpfWrapPanelTest.cs

--- WpfWrapPanelTest.cs
+++ WpfWrapPanelTest.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using Xunit;
 using Xunit.Abstractions;
