05bd9fce89115316dee74d283e58c93dda503430
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-05-31 18:19:01 +0200

    Update WpfGridTest.cs

--- WpfGridTest.cs
+++ WpfGridTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+﻿using System.Collections.Generic;
 using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Xunit;
