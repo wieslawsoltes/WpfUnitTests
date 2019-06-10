795e6b10ca76ef5a99369cc437036aadb3faf5a4
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2019-06-10 19:23:43 +0200

    Create FrameworkElementExtensions.cs

---
+++ tests/WpfUnitTests/FrameworkElementExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace WpfUnitTests
+{
+    public static class FrameworkElementExtensions
+    {
+        public static Rect BoundsRelativeTo(this FrameworkElement element, Visual relativeTo)
+        {
+            return element.TransformToVisual(relativeTo).TransformBounds(new Rect(element.RenderSize));
+        }
+
+        public static Rect BoundsRelativeTo(this UIElement element, Visual relativeTo)
+        {
+            return BoundsRelativeTo((FrameworkElement)element, relativeTo);
+        }
+    }
+}
